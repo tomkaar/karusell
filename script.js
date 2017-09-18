b915d133efd54b99be468966a7a4a352008ec701
leolainen <leolainen2@gmail.com>
2017-09-18 14:05:26 +0200

    case försök 1

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ window.onload = function() {
 
 	var currentSpin = 0;
 
+	var a1 = document.getElementsByClassName("a1");
+	var a2 = document.getElementsByClassName("a2");
+	var a3 = document.getElementsByClassName("a3");
+	var a4 = document.getElementsByClassName("a4");
+	var a5 = document.getElementsByClassName("a5");
+	var a6 = document.getElementsByClassName("a6");
+	var a7 = document.getElementsByClassName("a7");
+	var a8 = document.getElementsByClassName("a8");
+	var a9 = document.getElementsByClassName("a9");
+
 	left.onclick = function() {
 		console.log("left");
 		spinLeft();
@@ -25,5 +35,9 @@ window.onload = function() {
 		carousel.style.transform = "rotateY("+currentSpin+"deg)";
 	}
 
-
+	switch(currentSpin) {
+		case 0:
+			a1.style.transform = "scale(1.2)";
+			break;
+	}
 } // end onload
