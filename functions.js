6a8e0f9213ca72ff2e252d1f25fb6ac9d1d023bd
Tommy Kärnström <31956581+tomkaar@users.noreply.github.com>
2017-09-18 13:35:40 +0200

    CleanUp

--- functions.js
+++ functions.js
@@ -3,41 +3,37 @@ var right = document.getElementById("right");
 var carousel = document.getElementById("carousel");
 var spinDeg = 0;
 
-
+// Left button click
 left.onclick = function(){
 	spinLeft();
 }
+// Right Button click
 right.onclick = function(){
 	spinRight();
 }
-
-
+// Arrow Keydown
 document.onkeydown = checkKey;
-
-function checkKey(e) {
-
-   e = e || window.event;
-
-   if (e.keyCode == '38') {
-       console.log("Up");
-       spinZero();
-   }
-   else if (e.keyCode == '40') {
-       console.log("Down");
-       spinOneSixty();
-   }
-   else if (e.keyCode == '37') {
-      console.log("Left");
-      spinLeft();
-   }
-   else if (e.keyCode == '39') {
-      console.log("Right");
-      spinRight();
-   }
-
+	function checkKey(e) {
+	e = e || window.event;
+	if (e.keyCode == '38') {
+	    console.log("Up");
+	    spinZero();
+	}
+	else if (e.keyCode == '40') {
+	    console.log("Down");
+	    spinOneSixty();
+	}
+	else if (e.keyCode == '37') {
+	    console.log("Left");
+	    spinLeft();
+	}
+	else if (e.keyCode == '39') {
+	    console.log("Right");
+	    spinRight();
+	}
 }
 
-
+// Called Spin Fuctions
 function spinLeft(){
 	spinDeg += 40;
 	carousel.style.transform = "rotateY("+ spinDeg +"deg)"; 
