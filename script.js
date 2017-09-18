9e1911df5d10c0208f13006c549ba92a2c505106
leolainen <leolainen2@gmail.com>
2017-09-18 15:01:22 +0200

    case check försök 3
    misslyckat. måste försöka  nån annan metod

--- script.js
+++ script.js
@@ -27,19 +27,101 @@ window.onload = function() {
 
 	function spinLeft() {
 		currentSpin -= 40;
+		spinCheck();
 		carousel.style.transform = "rotateY("+currentSpin+"deg)";
 	}
 
 	function spinRight() {
-		currentSpin += 40;
+		currentSpin += 40;		
+		spinCheck();
 		carousel.style.transform = "rotateY("+currentSpin+"deg)";
 	}
 
-	switch(currentSpin) {
-		case 0:
-			a1.style.transform = "scale(1.6)";
-			break;
-		default:
-			console.log("wat?");
+	function spinCheck() {
+		switch(currentSpin) {
+			case 0:
+				if (currentSpin == 0) {
+					console.log(currentSpin);
+					a1.style.transform = "rotateY(0deg) translateZ(392px) scale(1.3)";
+				}
+				else {
+					a1.style.transform = "rotateY(0deg) translateZ(392px) scale(1)";
+				}	
+				break;
+			case 40:
+				if (currentSpin == 40) {
+					console.log(currentSpin);
+					a2.style.transform = "rotateY(40deg) translateZ(392px) scale(1.3)";
+				}
+				else {
+					a2.style.transform = "rotateY(40deg) translateZ(392px) scale(1)";
+				}	
+				break;
+			case 80:
+				if (currentSpin == 80) {
+					console.log(currentSpin);
+					a3.style.transform = "rotateY(80deg) translateZ(392px) scale(1.3)";
+				}
+				else {
+					a3.style.transform = "rotateY(80deg) translateZ(392px) scale(1)";
+				}	
+				break;
+			case 120:
+				if (currentSpin == 120) {
+					console.log(currentSpin);
+					a4.style.transform = "rotateY(120deg) translateZ(392px) scale(1.3)";
+				}
+				else {
+					a4.style.transform = "rotateY(120deg) translateZ(392px) scale(1)";
+				}	
+				break;
+			case 160:
+				if (currentSpin == 160) {
+					console.log(currentSpin);
+					a5.style.transform = "rotateY(160deg) translateZ(392px) scale(1.3)";
+				}
+				else {
+					a5.style.transform = "rotateY(160deg) translateZ(392px) scale(1)";
+				}	
+				break;
+			case 200:
+				if (currentSpin == 200) {
+					console.log(currentSpin);
+					a6.style.transform = "rotateY(200deg) translateZ(392px) scale(1.3)";
+				}
+				else {
+					a6.style.transform = "rotateY(200deg) translateZ(392px) scale(1)";
+				}	
+				break;
+			case 240:
+				if (currentSpin == 240) {
+					console.log(currentSpin);
+					a7.style.transform = "rotateY(240deg) translateZ(392px) scale(1.3)";
+				}
+				else {
+					a7.style.transform = "rotateY(240deg) translateZ(392px) scale(1)";
+				}	
+				break;
+			case 280:
+				if (currentSpin == 280) {
+					console.log(currentSpin);
+					a8.style.transform = "rotateY(280deg) translateZ(392px) scale(1.3)";
+				}
+				else {
+					a8.style.transform = "rotateY(280deg) translateZ(392px) scale(1)";
+				}	
+				break;
+			case 320:
+				if (currentSpin == 320) {
+					console.log(currentSpin);
+					a1.style.transform = "rotateY(320deg) translateZ(392px) scale(1.3)";
+				}
+				else {
+					a1.style.transform = "rotateY(320deg) translateZ(392px) scale(1)";
+				}	
+				break;
+			default:
+				console.log("wat?");
+		}	
 	}
 } // end onload
