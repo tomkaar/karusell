a73489be581daf3a2836c05bd490e0ea16d244f4
Tommy Kärnström <31956581+tomkaar@users.noreply.github.com>
2017-09-18 13:24:54 +0200

    ArrowKeys
    Added spin functions and arrow keys

--- functions.js
+++ functions.js
@@ -1,7 +1,56 @@
 var left = document.getElementById("left");
 var right = document.getElementById("right");
 var carousel = document.getElementById("carousel");
+var spinDeg = 0;
+
 
 left.onclick = function(){
-	carousel.style.transform = "rotateY(40deg)"; 
+	spinLeft();
+}
+right.onclick = function(){
+	spinRight();
+}
+
+
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+
+   e = e || window.event;
+
+   if (e.keyCode == '38') {
+       console.log("Up");
+       spinZero();
+   }
+   else if (e.keyCode == '40') {
+       console.log("Down");
+       spinOneSixty();
+   }
+   else if (e.keyCode == '37') {
+      console.log("Left");
+      spinLeft();
+   }
+   else if (e.keyCode == '39') {
+      console.log("Right");
+      spinRight();
+   }
+
+}
+
+
+function spinLeft(){
+	spinDeg += 40;
+	carousel.style.transform = "rotateY("+ spinDeg +"deg)"; 
+}
+function spinRight(){
+	spinDeg -= 40;
+	carousel.style.transform = "rotateY("+ spinDeg +"deg)"; 
+}
+function spinZero(){
+	spinDeg = 0;
+	carousel.style.transform = "rotateY("+ spinDeg +"deg)"; 
+}
+function spinOneSixty(){
+	spinDeg = 160;
+	carousel.style.transform = "rotateY("+ spinDeg +"deg)"; 
 }
