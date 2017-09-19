9be2b496a7df194bc96faf189a881a7544898fff
Tommy Kärnström <31956581+tomkaar@users.noreply.github.com>
2017-09-19 09:35:16 +0200

    Spin Function

--- functions.js
+++ functions.js
@@ -2,6 +2,12 @@ var left = document.getElementById("left");
 var right = document.getElementById("right");
 var carousel = document.getElementById("carousel");
 var spinDeg = 0;
+var curIndex = 0;
+
+var defaultTranslateZ = 392;
+var zoomTranslateZ = 592;
+
+currentIndex(0);
 
 // Left button click
 left.onclick = function(){
@@ -16,10 +22,10 @@ document.onkeydown = checkKey;
 	function checkKey(e) {
 	e = e || window.event;
 	if (e.keyCode == '38') {
-	    spinZero();
+	    spinRight();
 	}
 	else if (e.keyCode == '40') {
-	    spinOneSixty();
+	    spinLeft();
 	}
 	else if (e.keyCode == '37') {
 	    spinLeft();
@@ -33,16 +39,45 @@ document.onkeydown = checkKey;
 function spinLeft(){
 	spinDeg += 40;
 	carousel.style.transform = "rotateY("+ spinDeg +"deg)"; 
+	removeIndex();
 }
 function spinRight(){
 	spinDeg -= 40;
 	carousel.style.transform = "rotateY("+ spinDeg +"deg)"; 
+	addIndex();
 }
-function spinZero(){
-	spinDeg = 0;
-	carousel.style.transform = "rotateY("+ spinDeg +"deg)"; 
+
+// currentIndex
+function addIndex(){
+	curIndex += 1;
+	currentIndex(curIndex);
 }
-function spinOneSixty(){
-	spinDeg = 160;
-	carousel.style.transform = "rotateY("+ spinDeg +"deg)"; 
+function removeIndex(){
+	curIndex -= 1;
+	currentIndex(curIndex);
+}
+function currentIndex(cIndex){
+	if (cIndex > 8) {
+		curIndex = 0;
+	} else if( cIndex < 0){
+		curIndex = 8;
+	} else{
+		curIndex = cIndex;
+	}
+	
+	shrink();
+	grow(curIndex);
+}
+
+// Shrink Box
+function shrink(){
+	for(i = 0; i < 9; i++) { 
+		var thisElement = document.getElementById("a"+ i);
+		thisElement.style.transform = "rotateY("+ i * 40 +"deg) translateZ("+ defaultTranslateZ +"px)"; 
+	}
+}
+// Grow box
+function grow(index){
+	var thisElement = document.getElementById("a"+ index);
+	thisElement.style.transform = "rotateY("+ index * 40 +"deg) translateZ("+ zoomTranslateZ +"px)"; 
 }
